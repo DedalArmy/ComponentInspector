b4f450269d0496acefd868b5ff471355c0a4f23b
Alexandre Le Borgne <alexandre.leborgne@gmail.com>
2018-03-28 19:29:53 +0200

    getting jar and xml files from repositories _ get the lists

--- fr.ema.dedal.componentinspector/src/fr/ema/dedal/componentinspector/explorer/Explorer.java
+++ fr.ema.dedal.componentinspector/src/fr/ema/dedal/componentinspector/explorer/Explorer.java
@@ -1,5 +1,6 @@
 package fr.ema.dedal.componentinspector.explorer;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -20,9 +21,12 @@ import fr.ema.dedal.componentinspector.inspector.JarInspector;
 import dedal.impl.DedalFactoryImpl;
 
 public class Explorer {
-	
+
 	static final Logger logger = Logger.getLogger(Explorer.class);
-	
+	static final String xml = ".xml";
+	static final String jar = ".jar";
+	static final String war = ".war";
+
 	private Explorer() {}
 
 	/**
@@ -42,7 +46,7 @@ public class Explorer {
 			 */
 			DedalDiagram result = new DedalFactoryImpl().createDedalDiagram();
 			result.setName("genDedalDiag");
-			
+
 			inspectJar(singlePath, sdslPath, result);
 			return result;
 		} 
@@ -51,10 +55,11 @@ public class Explorer {
 			return null;
 		}
 	}
-	
+
 	public static List<DedalDiagram> generateAll(String libPath)
 	{
 		List<DedalDiagram> result = new ArrayList<>();
+		
 		/**
 		 * We know the library hierarchy tree
 		 * 
@@ -64,24 +69,50 @@ public class Explorer {
 		 * |	|
 		 * |	|----Repositories
 		 */
-		
 		try {
-			List<URI> owners = FolderLoader.loadFolder(Paths.get(libPath));
-			Map<URI, List<URI>> ownerToRepos = new HashMap<>();
-			for(URI o : owners)
+			List<URI> repos = new ArrayList<>();
+			for(URI o : FolderLoader.loadFolder(Paths.get(libPath)))
 			{
-				ownerToRepos.put(o, FolderLoader.loadFolder(Paths.get(o)));
+				repos.addAll(FolderLoader.loadFolder(Paths.get(o)));
 			}
 			if(logger.isInfoEnabled())
-				ownerToRepos.forEach((o,repos) -> repos.forEach(r -> logger.info(r.toString())));
+			{
+				repos.forEach(r -> logger.info(r.toString()));
+				logger.info("number of repositories that are going to be inspected : " + repos.size());
+			}
+			for(URI folder : repos)
+			{
+				File f = new File(folder);
+				if(f.isDirectory())
+				{
+					List<URI> xmlFiles = recursivelyGetFileURIs(folder, xml);
+					List<URI> jarFiles = recursivelyGetFileURIs(folder, jar, war);
+					URL[] urlsToLoad = new URL[jarFiles.size()];
+					for(int i = 0; i<jarFiles.size(); i++)
+					{
+						urlsToLoad[i]=jarFiles.get(i).toURL();
+					}
+					Map<URI, List<Class<?>>> classes;
+					JarLoader jarloarder = new JarLoader(urlsToLoad);
+					try
+					{
+						classes = jarloarder.loadClasses();
+					}catch (Exception e) {
+						logger.error(e.getMessage(),e);
+					}
+					 
+					System.out.println("coucou");
+				}
+			}
 			
+
 			return result;
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
 		return Collections.emptyList();
 	}
-	
+
 	/**
 	 * Loads and inspects the jar file.
 	 * @param singlePath
@@ -93,7 +124,7 @@ public class Explorer {
 			throws MalformedURLException {
 		URL[] urlToLoad=new URL[]{Paths.get(singlePath).toUri().toURL()};
 		JarLoader jarloader = new JarLoader(urlToLoad);
-		
+
 		/**
 		 * Let's extract some component classes.
 		 */
@@ -101,4 +132,31 @@ public class Explorer {
 		jarInspector.generate(result, sdslPath);
 	}
 	
+	/**
+	 * 
+	 * @param folder
+	 * @return
+	 * @throws IOException 
+	 */
+	private static List<URI> recursivelyGetFileURIs(URI folder, String ... fileExtensions) throws IOException
+	{
+		List<URI> result = new ArrayList<>();
+		List<URI> tempURIs = FolderLoader.loadFolder(Paths.get(folder));
+		for(URI uri : tempURIs)
+		{
+			File f = new File(uri);
+			if(f.isFile())
+				for(String ext : fileExtensions)
+				{
+					if(f.getName().endsWith(ext))
+						result.add(uri);
+				}
+			else if(f.isDirectory())
+			{
+				result.addAll(recursivelyGetFileURIs(uri, fileExtensions));
+			}
+		}
+		return result;
+	}
+
 }
